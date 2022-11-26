8232e1330266eabdc6d5f5882cb19817d7f7cd31
Brian S <briansukhnandan@gmail.com>
2022-11-25 19:10:08 -0500

    Turn HomePage into a functional component instead of class since we are not tracking state

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,23 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ShowCarousel from './shows/ShowCarousel';
 import Api from '../api/Api';
 
-export default class HomePage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  async componentDidMount() {
-
-  }
-
-  render() {
-    return(
-      <div>
-        <ShowCarousel 
-          api={new Api()}
-        />
-      </div>
-    )
-  }
+export default function HomePage() {
+  return (
+    <>
+      <ShowCarousel 
+        api={new Api()}
+      />
+    </>
+  );
 }
