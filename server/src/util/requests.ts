911c44240d8ec90a5d8459260a838f16341348b1
Brian Sukhnandan <briansukhnandan@gmail.com>
2023-05-27 04:27:21 -0400

    remove old file

--- server/src/util/requests.ts
+++
@@ -1,7 +0,0 @@
-// Adds on extra args to request string if necessary.
-export function appendToEndpoint(endpoint: string, modifier: string) {
-  const endpointParamSplit = endpoint.split('?');
-  endpointParamSplit[0] = `${endpointParamSplit[0]}${modifier}`;
-
-  return endpointParamSplit.join('?');
-}
